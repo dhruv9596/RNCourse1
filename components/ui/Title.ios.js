9fc45972257f4614e905a4b3e21f017780fc0026
dhruv9596 <dhruvlodaliya9596@gmail.com>
2023-02-17 09:53:58 +0530

    First Commit

---
+++ components/ui/Title.ios.js
@@ -0,0 +1,25 @@
+import { Text, StyleSheet , Platform } from 'react-native';
+
+function Title({ children }) {
+  return <Text style={styles.title}>{children}</Text>;
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 24,
+    // fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+    //borderWidth: Platform.OS === 'android' ? 2 : 0,
+    //borderWidth : Platform.select( { ios : 0 , android : 2 }),
+    borderWidth : 0,
+    borderColor: 'white',
+    padding: 12, 
+    maxWidth : '80%',
+    width : 300,
+    marginTop : 50
+  },
+});
